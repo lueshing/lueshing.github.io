c1e3e64c669df6cb6dbd3660a5e166d42fb8390c
lueshing <lueshing@hotmail.com>
2025-06-04 12:33:51 -0400

    Add files via upload

---
+++ links.js
@@ -0,0 +1,142 @@
+// FIRST ROW
+headingArray1 = new Array();
+ subCatArray1 = new Array();
+
+hA = headingArray1;
+sA = subCatArray1;
+i = 0; 
+
+hA[i] = "Entertainment";
+sA[i] = new Array();
+sA[i].push(new Array("Billboard.com", "http://www.billboard.com/"));
+sA[i].push(new Array("Netflix", "https://www.netflix.com/ca/"));
+sA[i].push(new Array("Rogers Anyplace", "http://www.rogersondemand.com/"));
+
+i++;
+
+hA[i] = "Movie Stuff";
+sA[i] = new Array();
+sA[i].push(new Array("IMDB.com", "http://www.imdb.com"));
+sA[i].push(new Array("movies.com", "http://www.movies.com"));
+sA[i].push(new Array("rottentomatoes.com", "http://www.rottentomatoes.com/"));
+sA[i].push(new Array("Yahoo Movies", "http://movies.yahoo.com/"));
+i++;
+
+hA[i] = "News";
+sA[i] = new Array();
+sA[i].push(new Array("680news", "http://www.680news.com/"));
+sA[i].push(new Array("ABCNews", "http://abcnews.go.com/"));
+sA[i].push(new Array("CBC", "http://www.cbc.ca/"));
+sA[i].push(new Array("CNN", "http://www.cnn.com"));
+sA[i].push(new Array("Toronto Star", "http://www.thestar.com"));
+i++;      
+
+hA[i] = "Shopping";
+sA[i] = new Array();
+sA[i].push(new Array("amazon.ca", "http://www.amazon.ca"));
+sA[i].push(new Array("canadiantire.ca", "http://www.canadiantire.ca"));
+sA[i].push(new Array("eBay.ca", "http://www.ebay.ca"));
+sA[i].push(new Array("kijiji Toronto", "http://toronto.kijiji.ca/"));
+sA[i].push(new Array("redflagdeals.com", "http://www.redflagdeals.com"));
+sA[i].push(new Array("Staples", "http://www.staples.ca"));
+i++;
+
+hA[i] = "Gift Cards";
+sA[i] = new Array();
+sA[i].push(new Array("Cardswap", "http://www.cardswap.ca"));
+sA[i].push(new Array("Ebates Gift Cards", "https://www.ebatesgiftcards.ca/"));
+sA[i].push(new Array("Gift Cards Wild", "http://www.giftcardswild.com/"));
+i++ 
+
+hA[i] = "Sports";
+sA[i] = new Array();
+sA[i].push(new Array("TSN", "http://www.tsn.ca/"));
+sA[i].push(new Array("NHL", "http://www.nhl.com/"));
+sA[i].push(new Array("MLB", "http://www.mlb.com/"));
+sA[i].push(new Array("NBA", "http://www.nba.com/"));
+sA[i].push(new Array("ESPN", "http://www.espn.com"));
+sA[i].push(new Array("Sportsnet.ca", "http://www.sportsnet.ca/"));
+sA[i].push(new Array("Yahoo Sports", "http://sports.yahoo.com"));
+i++;
+
+// SECOND ROW
+headingArray2 = new Array();
+ subCatArray2 = new Array();
+
+hA = headingArray2;
+sA = subCatArray2;
+i = 0; 
+
+hA[i] = "Computer Stores";
+sA[i] = new Array();
+sA[i].push(new Array("CanadaComputers", "http://www.canadacomputers.com/"));
+i++;  
+
+hA[i] = "E-mail Sites";
+sA[i] = new Array();
+sA[i].push(new Array("GMail", "http://www.gmail.com/"));
+sA[i].push(new Array("Hotmail", "http://www.hotmail.com"));
+i++     
+      
+hA[i] = "Misc";
+sA[i] = new Array();
+sA[i].push(new Array("Koodo", "https://www.koodomobile.com/my-account/overview?INTCMP=KMNew_NavMenu_SelfServe"));
+sA[i].push(new Array("Public Mobile", "https://selfserve.publicmobile.ca"));
+i++;
+
+hA[i] = "Financial Stuff";
+sA[i] = new Array();
+sA[i].push(new Array("CanadianTire Mastercard", "https://www.ctfs.com/MyOnlineAccount/"));
+sA[i].push(new Array("Globeinvestor", "http://www.globeinvestor.com/"));
+sA[i].push(new Array("Paypal", "http://www.paypal.com/"));
+sA[i].push(new Array("PC Optimum", "https://www.pcoptimum.ca"));
+sA[i].push(new Array("Scotiabank", "http://www.scotiabank.com"));
+sA[i].push(new Array("Sunlife", "http://www.sunlife.ca/member/"));
+sA[i].push(new Array("TD EasyWeb", "https://easyweb.td.com"));
+i++;
+
+
+hA[i] = "Search Sites";
+sA[i] = new Array();
+sA[i].push(new Array("bing.com", "http://www.bing.com/"));
+sA[i].push(new Array("dictionary.com", "http://www.dictionary.com"));
+sA[i].push(new Array("Google Maps", "http://maps.google.com/"));
+i++;
+
+hA[i] = "Bills";
+sA[i] = new Array();
+sA[i].push(new Array("Hydro One", "https://www.hydroone.com/#"));
+sA[i].push(new Array("Enbridge", "https://www.enbridgegas.com"));
+sA[i].push(new Array("Prop Tax/Water", "www.eastgwillimbury.ca/Services/Property_Taxes_and_Water_Sewer_Bills.htm"));
+i++;
+
+// THIRD ROW
+headingArray3 = new Array();
+subCatArray3  = new Array();
+
+hA = headingArray3;
+sA = subCatArray3;
+i = 0; 
+
+hA[i] = "Funny Stuff";
+sA[i] = new Array();
+sA[i].push(new Array("Urban Dictionary", "http://www.urbandictionary.com/"));
+i++;  
+
+hA[i] = "Multimedia Sites";
+sA[i] = new Array();
+sA[i].push(new Array("Google Videos", "http://video.google.ca/"));
+sA[i].push(new Array("YouTube", "http://www.youtube.com/"));
+
+i++;  
+        
+
+hA[i] = "Networking Stuff";
+sA[i] = new Array();
+sA[i].push(new Array("Facebook", "http://www.facebook.com"));
+i++;
+
+hA[i] = "Weather";
+sA[i] = new Array();
+sA[i].push(new Array("weather.com (Holland Landing)", "https://www.theweathernetwork.com/ca/weather/ontario/holland-landing"));
+i++
